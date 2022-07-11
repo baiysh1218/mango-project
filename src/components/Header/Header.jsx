5b76f590e1225c881ded37121f4ef8a3add8bd4c
baiysh1218 <baiyshbeksultanov1@gmail.com>
2022-07-11 23:20:39 +0600

    added navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+import "./Header.css";
+
+import SearchIcon from "@mui/icons-material/Search";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+import { IconButton } from "@mui/material";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="navber-background">
+      <div className="container">
+        <div className="navbar-block">
+          <div className="navbar-logo-block">
+            <img
+              className="logo-mango-navbar"
+              src="https://st.mngbcn.com/images/headerNew/logos/mango.svg"
+              alt=""
+            />
+          </div>
+          <div className="navbar-block-icons">
+            <IconButton>
+              <SearchIcon className="navbar-icon" />
+            </IconButton>
+            <IconButton>
+              <PermIdentityIcon className="navbar-icon" />
+            </IconButton>
+            <IconButton>
+              <FavoriteBorderIcon className="navbar-icon" />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
