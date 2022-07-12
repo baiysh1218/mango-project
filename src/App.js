3c07c729fd2c5fb463ebe2fcf965917ece5f3d56
Nursultan Amanturov <nursultan.amanturov.kk@gmail.com>
2022-07-12 15:10:25 +0600

    add context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
+import ProductsContextProvider from "./contexts/productContetx";
+import Routing from "./Routing/Routing";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <ProductsContextProvider>
+      <BrowserRouter>
+        <Header />
+        <Routing />
+      </BrowserRouter>
+    </ProductsContextProvider>
   );
 };
 
