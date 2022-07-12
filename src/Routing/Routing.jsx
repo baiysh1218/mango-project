fa1d5015d8a5570c724500097e07c8f0ac453749
baiysh1218 <baiyshbeksultanov1@gmail.com>
2022-07-12 15:30:43 +0600

    added contexts

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Intro from "../components/Intro/Intro";
 
 const Routing = () => {
-  return <Routes>
-    <Route path="/" element={<Intro />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Intro />} />
+    </Routes>
+  );
 };
 
 export default Routing;
